81613c764726fd79ece47ceceed477b6d83ffc45
Pueh Leng Tan <puehleng.tan@gmail.com>
2023-11-25 16:55:53 +0100

    added maxgap docstring to unconfuse future me.

--- yellinpmax/maxgap.py
+++ yellinpmax/maxgap.py
@@ -3,15 +3,23 @@ import numpy as np
 #### Functions to compute yellin max gap ts
 def compute_c0_inner(x, mu):
     """
-    Helper function to compute Yellin max gap, see eq 2 of arxiv 0203002v2
+    Helper function to compute the probability of the max gap size, x_max, 
+    being smaller than some particular value of x, P(x_max<x) = c0 = 1-pvalue.
+    
+    Essentially computing the c0(x,mu) object in eq 2 of arxiv 0203002v2. Paper
+    says, 'For a 90% CL UL, increase xsec until mu and the observed x are such
+    that c0 reaches 0.90.'
+
+    Eg. If c0 = 0.9, one can reject that particular hypothesis (usually some
+    xsec that gives that particular signal spectrum) at 90% CL.
 
     Arguments:
-    x ():
-    mu ():
+    x (np.float64): Maximum gap size. Ie. Signal expectation in max gap
+    mu (np.float64): Total signal expectation in ROI
 
     Returns:
-    bag ():
-
+    bag (np.float64): Probability of the maximum gap size, x, being smaller 
+                      than a particular value of x
     """
 
     k_max = int(np.floor(mu/x))
@@ -27,16 +35,25 @@ def compute_c0_inner(x, mu):
 
 def compute_c0(f_cdf, input_events, s2_roi):
     """
+    Function to find the maximum gap given a signal spectrum and a set of
+    events, and compute the probability of that particular max gap being smaller
+    than some particular value of x.
+
+    Eg. If c0 = 0.9, one can reject that particular hypothesis (usually some
+    xsec that gives that particular signal spectrum) at 90% CL.
 
     Arguments:
-    f_cdf ():
-    input_events ():
-    s2_roi ():
+    f_cdf (scipy.interpolate.interpolate.interp1d): Interpolated cumulative
+                                                    function of input spectrum
+    input_events (np.array): Array of of observed events 
+    s2_roi (np.array, list, tuple): 2-element array or tuple indicating start and end point of
+                                    region-of-interest.
 
     Returns:
-
-
+    c0 (np.float64): P(x_max<x) = 1-pvalue
+    overall_mu (np.float64): Total signal expectation in ROI
     """
+
     # Compute overall mu
     overall_mu = f_cdf(s2_roi[1])-f_cdf(s2_roi[0])
     
